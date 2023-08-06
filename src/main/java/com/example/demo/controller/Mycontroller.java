c557cc01596e7cb74eba670e64175ccf434db7d4
thassan1977 <thairhasan1978@gmail.com>
2023-08-06 14:11:20 +0400

    test

---
+++ src/main/java/com/example/demo/controller/Mycontroller.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Mycontroller {
+
+    @GetMapping
+    public String getdata(){
+        return (new Date()).toString();
+    }
+}
